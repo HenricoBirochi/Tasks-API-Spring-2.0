7d5c47e63bd5e2fffd80cc7f641518e056a43806
henricobirochi <henrico.birochi@gmail.com>
2026-09-05 21:55:49 -0300

    feat: Classes Tag and TaskTag added to the domain

---
+++ src/main/java/com/henrico/tasks/domain/Tag.java
@@ -0,0 +1,9 @@
+package com.henrico.tasks.domain;
+
+public class Tag {
+
+    private String tagId;
+    private String title;
+    private String description;
+
+}
---
+++ src/main/java/com/henrico/tasks/domain/TaskTag.java
@@ -0,0 +1,9 @@
+package com.henrico.tasks.domain;
+
+public class TaskTag {
+
+    private String taskTagId;
+    private String tagId;
+    private String taskId;
+
+}
