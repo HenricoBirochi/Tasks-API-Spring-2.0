ba4aac3cc536cf1a927e60b522fb882929b13401
henricobirochi <henrico.birochi@gmail.com>
2026-09-05 22:00:27 -0300

    fix: add constructors and getters/setters to Tag and TaskTag classes

--- src/main/java/com/henrico/tasks/domain/Tag.java
+++ src/main/java/com/henrico/tasks/domain/Tag.java
@@ -6,4 +6,37 @@ public class Tag {
     private String title;
     private String description;
 
+    public Tag() {
+    }
+
+    public Tag(String tagId, String title, String description) {
+        this.tagId = tagId;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
--- src/main/java/com/henrico/tasks/domain/TaskTag.java
+++ src/main/java/com/henrico/tasks/domain/TaskTag.java
@@ -6,4 +6,37 @@ public class TaskTag {
     private String tagId;
     private String taskId;
 
+    public TaskTag() {
+    }
+
+    public TaskTag(String taskTagId, String tagId, String taskId) {
+        this.taskTagId = taskTagId;
+        this.tagId = tagId;
+        this.taskId = taskId;
+    }
+
+    public String getTaskTagId() {
+        return taskTagId;
+    }
+
+    public void setTaskTagId(String taskTagId) {
+        this.taskTagId = taskTagId;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
 }
