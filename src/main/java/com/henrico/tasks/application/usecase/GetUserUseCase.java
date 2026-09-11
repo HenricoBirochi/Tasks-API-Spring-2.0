dea6d2948345962f503dcf8a2a33b8f1c8035e36
henricobirochi <henrico.birochi@gmail.com>
2026-09-11 01:10:42 -0300

    feat: add GetUserUseCase interface for user retrieval functionality

---
+++ src/main/java/com/henrico/tasks/application/usecase/GetUserUseCase.java
@@ -0,0 +1,9 @@
+package com.henrico.tasks.application.usecase;
+
+import com.henrico.tasks.application.usecase.dto.output.UserOutput;
+
+public interface GetUserUseCase {
+
+    UserOutput getUser(String)
+
+}
